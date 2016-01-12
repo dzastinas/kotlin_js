a76c2a45c4a4b7597271857d8135f57de9253ab5
justinas.krutulis <justinas.krutulis@gmail.com>
2016-01-12 19:13:05 +0200

    Initial commit

---
+++ src/Hello.kt
@@ -0,0 +1,12 @@
+
+
+class Hello {
+    fun hello(): String {
+        return "hello"
+    }
+}
+
+fun main(args: Array<String>){
+    println("Hello World !!")
+}
+
---
+++ web/js/app/kotlin_js.js
@@ -0,0 +1,10 @@
+(function (Kotlin) {
+  'use strict';
+  var _ = Kotlin.defineRootPackage(null, /** @lends _ */ {
+    main_kand9s$: function (args) {
+      Kotlin.println('Hello World !!');
+    }
+  });
+  Kotlin.defineModule('kotlin_js', _);
+  _.main_kand9s$([]);
+}(Kotlin));
